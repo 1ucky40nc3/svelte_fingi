b702160c90841a508e2bd802d92f72eee6ff4f39
1ucky40nc3 <louisnwendler@gmail.com>
2025-09-12 20:15:06 +0200

    change app state documentation

--- src/lib/stores/app.ts
+++ src/lib/stores/app.ts
@@ -1,6 +1,7 @@
-// src/lib/stores.ts
 import type { TAppState } from '$lib/types/app';
 import { writable, type Writable } from 'svelte/store';
 
-// A writable store for the current count
+/**
+ * A writable store that holds the current state of the application.
+ */
 export const appState: Writable<TAppState> = writable<TAppState>('bluetoothSetup');
--- src/lib/stores/connection.ts
+++ src/lib/stores/connection.ts
@@ -1,5 +1,6 @@
-// src/lib/stores.ts
 import { writable, type Writable } from 'svelte/store';
 
-// A writable store for the current count
+/**
+ * A writable store that indicates if we are connected to a sensor device.
+ */
 export const isConnected: Writable<boolean> = writable<boolean>(false);
