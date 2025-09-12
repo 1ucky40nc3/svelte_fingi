f9fa33fb3d204a5948367b3a367dcecac49e40e6
1ucky40nc3 <louisnwendler@gmail.com>
2025-09-12 20:08:25 +0200

    navigator transitions are working

--- src/lib/components/NavigationNotification.svelte
+++ src/lib/components/NavigationNotification.svelte
@@ -1,45 +1,81 @@
 <script lang="ts">
 	import { appState } from '$lib/stores/app';
 	import { isConnected } from '$lib/stores/connection';
+	import type { TAppState } from '$lib/types/app';
+	import type { TAppStateTransitionDirections } from '$lib/types/transition';
 	import { AppStateTransitionDirectionsFactory } from '$lib/utils/transitions';
 	import Disconnected from './Disconnected.svelte';
 	import NextTransition from './NextTransition.svelte';
 	import PreviousTransition from './PreviousTransition.svelte';
 
-	const transitions = AppStateTransitionDirectionsFactory.for($appState);
-	let prevTransitionAvailable = transitions.prev.isAvailable($isConnected);
-	let nextTransitionAvailable = transitions.next.isAvailable($isConnected);
+	// Implement the transitions based on the current app state.
+	let transitions: TAppStateTransitionDirections<TAppState>;
+	let currentPreviousTransitionComponent: typeof PreviousTransition | null = null;
+	let currentNextTransitionComponent: typeof NextTransition | null = null;
+	let prevTransitionHandler: () => void;
+	let prevText: string;
+	let nextTransitionHandler: () => void;
+	let nextText: string;
+	$: {
+		transitions = AppStateTransitionDirectionsFactory.for($appState);
+		const prevTransitionAvailable: boolean = transitions.prev.isAvailable($isConnected);
+		const nextTransitionAvailable: boolean = transitions.next.isAvailable($isConnected);
 
-	function prevTransitionHandler(): void {
-		if (transitions.prev !== undefined) {
-			transitions.prev.handleTransition(appState);
+		if (prevTransitionAvailable) {
+			currentPreviousTransitionComponent = PreviousTransition;
+		} else {
+			currentPreviousTransitionComponent = null;
+		}
+		if (nextTransitionAvailable) {
+			currentNextTransitionComponent = NextTransition;
+		} else {
+			currentNextTransitionComponent = null;
+		}
+
+		prevTransitionHandler = (): void => {
+			if (transitions.prev !== undefined) {
+				transitions.prev.handleTransition(appState);
+			}
+		};
+		prevText = transitions.prev.text;
+
+		nextTransitionHandler = (): void => {
+			if (transitions.next !== undefined) {
+				transitions.next.handleTransition(appState);
+			}
+		};
+		nextText = transitions.next.text;
+	}
+	$: console.log(`Current prexText: ${prevText}; Current nextText: ${nextText}`);
+
+	// Implement the connection status
+	let currentDisconnectedComponent: typeof Disconnected | null = null;
+	$: {
+		if (!$isConnected) {
+			currentDisconnectedComponent = Disconnected;
+		} else {
+			currentDisconnectedComponent = null;
 		}
 	}
-	const prevText: string = transitions.prev.text;
-
-	function nextTransitionHandler(): void {
-		if (transitions.next !== undefined) {
-			transitions.next.handleTransition(appState);
-		}
-	}
-	const nextText: string = transitions.next.text;
+	$: console.log(`Connection status changed: ${$isConnected}`);
 </script>
 
 <div class="h-gr-1 flex flex-row items-center justify-between">
 	<div>
-		{#if prevTransitionAvailable}
-			<PreviousTransition transitionHandler={prevTransitionHandler} text={prevText}
-			></PreviousTransition>
-		{/if}
+		<svelte:component
+			this={currentPreviousTransitionComponent}
+			transitionHandler={prevTransitionHandler}
+			text={prevText}
+		/>
 	</div>
 
-	{#if !$isConnected}
-		<Disconnected></Disconnected>
-	{/if}
+	<svelte:component this={currentDisconnectedComponent} />
 
 	<div>
-		{#if nextTransitionAvailable}
-			<NextTransition transitionHandler={nextTransitionHandler} text={nextText}></NextTransition>
-		{/if}
+		<svelte:component
+			this={currentNextTransitionComponent}
+			transitionHandler={nextTransitionHandler}
+			text={nextText}
+		/>
 	</div>
 </div>
